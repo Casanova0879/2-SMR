6e031a947839f68501d5809b6b6226a2842ea240
Casanova <alvaro.casanova@davante.org>
2026-09-21 18:36:20 +0200

    Ejercicios

---
+++ Ejercicios Bloque 1/HolaSMR.java
@@ -0,0 +1,8 @@
+public class HolaSMR {
+    public static void main(String[] args) {
+        System.out.println("Hola,SMR");
+        System.out.println("Segundo Curso");
+        System.out.println("Fundamentos de programación");
+    }
+    
+}
